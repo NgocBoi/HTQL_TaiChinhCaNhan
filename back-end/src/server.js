fbda5f1cc9b2a2b539cb97e3812c6da3adc93fd8
Ngoc Thuong <NgocThuong@example>
2026-06-19 02:24:00 +0700

    Cap nhat de buid docker

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -10,9 +10,10 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   await connectDB();
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT} [${process.env.NODE_ENV || 'development'}]`);
-  });
+ 
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT} [${process.env.NODE_ENV || 'development'}]`);
+});
 };
 
 startServer();
